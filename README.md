0a29b0f0b237d72d5bd0fa8b23ec2c80aaddcf7d
Sushant Malvadakar <67320498+sushant-sm@users.noreply.github.com>
2022-06-15 13:11:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# laravel-8-jwt-token
+1) composer require tymon/jwt-auth
+2) php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"
+3) php artisan jwt:secret
+4) php artisan make:controller ApiController
+5) php artisan make:model Product -rcm
+6) php artisan migrate
---
+++ database/migrations/2022_06_15_065956_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('restrict');
+            $table->string('name');
+            $table->string('sku');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
